1148fc37ca4d1acce75f337d7c660e3106986352
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-04-15 10:30:44 +0200

    exercise a,b, and c was successfuly soleved

--- LabExercise.Tests/FlickFlack/FlickFlackTests.cs
+++ LabExercise.Tests/FlickFlack/FlickFlackTests.cs
@@ -8,6 +8,69 @@ public class FlickFlackTests
     private const string Flack = "Flack";
     private const string FlickFlack = "FlickFlack";
     private readonly GetFlickFlackQueryHandler _handler = new();
+    
+    // Aufgabe a) 
+    
+    [Fact]
+    public async void ErrorWhenEndGreaterThen100()
+    {
+        // Arrange 
+        var request = new GetFlickFlackQueryRequest { Start = 20, End = 110 };
+
+        // Act
+        var action = () => _handler.Handle(request, new CancellationToken());
+
+        // Assert
+        await action.Should().ThrowExactlyAsync<ArgumentException>();
+    }
+    
+    [Fact]
+    public async void ErrorWhenStartGreaterEnd()
+    {
+        // Arrange 
+        var request = new GetFlickFlackQueryRequest { Start = 20, End = 19 };
+
+        // Act
+        var action = () => _handler.Handle(request, new CancellationToken());
+
+        // Assert
+        await action.Should().ThrowExactlyAsync<ArgumentException>();
+    }
+    
+    // Aufgabe b)
+
+    [Fact]
+    public async void ErrorWhenStartSmallerThanOne()
+    {
+        // Arrange
+        var request = new GetFlickFlackQueryRequest { Start = 0, End = 30 };
+        var handler = new GetFlickFlackQueryHandler();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<ArgumentException>(() => handler.Handle(request, new CancellationToken()));
+        Assert.Equal("Ungültige Eingabe, Start darf nicht weniger als 1 sein!", exception.Message);
+    }
+
+    [Fact]
+    public async void ErrorWhenEndBiggerThanHundert()
+    {
+        var request = new GetFlickFlackQueryRequest(){Start = 20, End = 110};
+        var handler = new GetFlickFlackQueryHandler();
+
+        var exception =
+            await Assert.ThrowsAsync<ArgumentException>((() => handler.Handle(request, new CancellationToken())));
+        Assert.Equal("Falsche Eingabe: End darf nicht mehr als 100 sein!", exception.Message);
+    }
+    [Fact]
+    public async void ErrorWhenStartIsBiggerThanEnd()
+    {
+        var request = new GetFlickFlackQueryRequest() { Start = 50, End = 49 };
+        var handler = new GetFlickFlackQueryHandler();
+        
+        var expection = await Assert.ThrowsAsync<ArgumentException>((() =>
+            handler.Handle(request, new CancellationToken())));
+        Assert.Equal("Falsche Eingabe: Start darf nicht größer als End sein!", expection.Message);
+    }
 
     [Fact]
     public async void ErrorWhenStartLessThen1()
--- LabExercise.Web/Features/FlickFlack/GetFlickFlackQuery.cs
+++ LabExercise.Web/Features/FlickFlack/GetFlickFlackQuery.cs
@@ -8,15 +8,16 @@ public class GetFlickFlackQueryRequest : IRequest<GetFlickFlackQueryResponse>
     public bool GetFlickFlackQuery()
     {
         const int maxOfListElements = 100;
-        const int minOfListElements = 0;
+        const int minOfListElements = 1;
 
-        if ((End <= maxOfListElements && Start > minOfListElements) && Start >= End)
+        if (End > maxOfListElements || Start < minOfListElements || Start > End)
+        {
+            throw new ArgumentException("Ungültige Eingaben für Start und End.");
+        }
+        else
         {
             return true;
         }
-        else return false;
-
-
     }
 }
 
@@ -34,24 +35,56 @@ public class GetFlickFlackQueryHandler : IRequestHandler<GetFlickFlackQueryReque
     {
         // TODO: Implement FlickFlack Request checks
         
+        if (request.Start < 1)
+        {
+            throw new ArgumentException("Ungültige Eingabe, Start darf nicht weniger als 1 sein!");
+        }
+        else if (request.End > 100)
+        {
+            throw new ArgumentException("Falsche Eingabe: End darf nicht mehr als 100 sein!");
+        }
+        else if (request.End < request.Start)
+        {
+            throw new ArgumentException("Falsche Eingabe: Start darf nicht größer als End sein!");
+        }
+        
         
         var result = new GetFlickFlackQueryResponse();
+            
         // TODO: Implement FlickFlack logic
         int number = 3;
         int number2 = 4;
+        int number3 = number * number2;
         var start = request.Start;
         var end = request.End;
-        while (request.GetFlickFlackQuery())
+        
+        if (request.GetFlickFlackQuery())
         {
             for (int i = start; i <= end; i++)
             {
                 
+                if (i % number3 == 0)
+                {
+                    result.Elements.Add("FlickFlack");
+                }
+                else if (i % number == 0)
+                {
+                    result.Elements.Add("Flick");
+                }
+                else if (i % number2 == 0)
+                {
+                    result.Elements.Add("Flack");
+                }
+                else
+                {
+                    
+                     result.Elements.Add(i.ToString());
+                }
+                
             }
             
         }
-
-        
-
         return Task.FromResult(result);
     }
+    
 }
